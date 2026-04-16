36a0f76e0d0468d66c7b04c2b8cfb6755a547e72
krishn404 <maharshikrishnakant@gmail.com>
2026-04-16 16:30:54 +0530

    revamp hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -27,8 +27,6 @@ export default function HeroSection() {
 
   return (
     <section className="relative w-full min-h-screen bg-background text-foreground overflow-hidden px-6 py-10">
-      
-      
 
       {/* logo + time */}
       <div className="absolute top-6 left-6 z-20">
@@ -37,12 +35,13 @@ export default function HeroSection() {
         <p className="text-[10px] opacity-70">{time}</p>
       </div>
 
-      {/* center image */}
+      {/* center images */}
       <div className="absolute inset-0 flex items-center justify-center">
         <motion.div
           initial={{ opacity: 0, scale: 0.95 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.6 }}
+          className="flex flex-col items-center"
         >
           <Image
             src="/stickers/me.png"
@@ -52,21 +51,24 @@ export default function HeroSection() {
             priority
             className="grayscale object-contain"
           />
+
+          <Image
+            src="/hero.png"
+            alt="hero"
+            width={500}
+            height={300}
+            className="object-contain mt-4"
+          />
         </motion.div>
       </div>
 
-    
-
       {/* floating labels */}
       <div className="absolute inset-0 pointer-events-none">
-        <span className="absolute left-[28%] top-[40%] text-xs">Creator *</span>
-        <span className="absolute left-[35%] top-[60%] text-xs">Stylist *</span>
-        <span className="absolute left-[50%] top-[35%] text-xs">DJ *</span>
-        <span className="absolute right-[30%] top-[40%] text-xs">Model *</span>
-        <span className="absolute right-[35%] top-[60%] text-xs">Girl *</span>
+        <span className="absolute left-[28%] top-[40%] text-xs">visual artist *</span>
+        <span className="absolute left-[35%] top-[60%] text-xs">music consumer *</span>
+        <span className="absolute right-[30%] top-[40%] text-xs">doom scroller *</span>
+        <span className="absolute right-[35%] top-[60%] text-xs">explorer *</span>
       </div>
-
-     
 
       {/* subtle border frame */}
       <div className="absolute inset-4 border border-pink-400 pointer-events-none" />
